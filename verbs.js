ec69e5eb3b24a876d261a7af31320730b90b532a
4N0NYM0U5-H4CK1V15T <41744738+4N0NYM0U5-H4CK1V15T@users.noreply.github.com>
2018-08-29 15:55:52 +0100

    Add a few verbs and the JQuery to substitute the French stems.

--- verbs.js
+++ verbs.js
@@ -1,14 +1,37 @@
+var verbs = ["parler", "jouer", "aimer"];
+var verbIndex = -1;
+
+function nextDifferentVerbIndex() {
+	function nextVerbIndex() {
+		return Math.floor((Math.random() * verbs.length));
+	}
+	var i=nextVerbIndex();
+	while (i == verbIndex) // Keep calling nextVerbIndex() until you get a new value
+		i=nextVerbIndex();
+	verbIndex = i;
+	return i;
+}
+
+function updateVerbTable(verb) {
+	var stem = verb.substring(0, verb.length - 2);
+	$(".stem").html(stem);
+}
+
 $(function() { // This code will be called when the document has loaded.
 	console.log("The document has loaded.");
 	$(".ending").each(
 		function(idx) {
-			console.log(idx);
-			$(this).prepend(idx + ": ");
 		}
 	);
 	$("#next").click(function() {
-		console.log("Ouch!");
+		verbIndex = nextDifferentVerbIndex();
+		console.log("You get " + verbIndex +" , which is " + verbs[verbIndex]);
+		updateVerbTable(verbs[verbIndex]);
 	});
+	
+	for (var i=0; i<verbs.length; i++)
+		console.log(verbs[i]);
 }
 );
+
 console.log("Hello world");
